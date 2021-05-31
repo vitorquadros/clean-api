ffa95dc871dd5e35b0174ee1ad2ac630bdca69b0
Vitor Quadros <viitorquadros@hotmail.com>
2021-05-31 17:56:06 -0300

    fix: return account

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -23,6 +23,6 @@ export class DbAddAccount implements AddAccount {
     const account = await this.addAccountRepository.add(
       Object.assign({}, accountData, { password: hashedPassword })
     );
-    return new Promise((resolve) => resolve(account));
+    return account;
   }
 }
