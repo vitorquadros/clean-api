9704c9eb48e4bdae623a97b7ae88a61e743ecbd9
Vitor Quadros <viitorquadros@hotmail.com>
2021-05-31 17:01:57 -0300

    fix: add await to add function

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -20,7 +20,7 @@ export class DbAddAccount implements AddAccount {
 
   async add(accountData: AddAccountModel): Promise<AccountModel> {
     const hashedPassword = await this.encrypter.encrypt(accountData.password);
-    this.addAccountRepository.add(
+    await this.addAccountRepository.add(
       Object.assign({}, accountData, { password: hashedPassword })
     );
     return new Promise((resolve) => resolve(null));
