62b245b1a60795ab263d5086be0b9a5bf05f474c
Vitor Quadros <viitorquadros@hotmail.com>
2021-06-07 10:43:13 -0300

    feat: ensure AccountMongoRepository returns an account on success

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  watchPathIgnorePatterns: ['globalConfig'],
   coverageDirectory: 'coverage',
-  testEnvironment: 'node',
   preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest',
